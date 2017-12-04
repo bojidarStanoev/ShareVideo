1559189f0df28d50cdeb6cda09b054a4ab5618b2
jobo <bojidar.stan@gmail.com>
2017-12-04 20:27:49 +0200

    adding search YT function

--- code/bot.rb
+++ code/bot.rb
@@ -1,19 +1,24 @@
 require 'facebook/messenger'
 require 'httparty' # you should require this one
 require 'json' # and that one
-require 'google_custom_search_api'
 include Facebook::Messenger
+require 'google/api_client'
+require 'trollop'
 # NOTE: ENV variables should be set directly in terminal for testing on localhost
 
 # Subcribe bot to your page
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+# Set DEVELOPER_KEY to the API
+DEVELOPER_KEY = 'AIzaSyAk0bdRE1Uw3O07roXwWBZyStsM-TXmkVA'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 hello_init=["hi","hello","zdr"]
 
 
 Bot.on :message do |message|
   puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
-  #parsed_response = get_parsed_response(API_URL, message.text) # talk to Google API
+  
   normal_msg = normalize(message)
   puts normal_msg
   if(hello_init.include?(normal_msg))
@@ -22,11 +27,22 @@ Bot.on :message do |message|
   
   elsif normal_msg.split(' ').first == "search"
     search = normal_msg.split(' ')[1..-1].join(' ')
-    results = GoogleCustomSearchApi.search(search) 
-      results.items.each do |item|
-        message.reply(text:  item.title)
-         message.reply(text:  item.link) 
-      end
+      get_search_res=find_video(search).first
+      puts get_search_res
+       message.reply(
+  attachment: {
+    "type": "template",
+    "payload": {
+      "template_type": "open_graph",
+                   "elements": [
+                       {
+                           "url": binding.local_variable_get("get_search_res")
+                       }
+                   ]
+    }
+  }
+)
+     
     
   else
     
@@ -39,5 +55,50 @@ end
 
 def normalize(message)
   message = message.text.downcase
-  
+end
+
+def get_service
+  client = Google::APIClient.new(
+    :key => DEVELOPER_KEY,
+    :authorization => nil,
+    :application_name => $PROGRAM_NAME,
+    :application_version => '1.0.0'
+  )
+  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+  return client, youtube
+end
+
+
+
+def  find_video(search)
+
+  opts = Trollop::options do
+    opt :q, 'Search term', :type => String, :default => search
+    opt :max_results, 'Max results', :type => :int, :default => 2
+  end
+  client, youtube = get_service
+
+  search_response = client.execute!(
+      :api_method => youtube.search.list,
+      :parameters => {
+        :part => 'snippet',
+        :q => opts[:q],
+        :maxResults => opts[:max_results]
+      }
+    )
+
+    videos = []
+
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    search_response.data.items.each do |search_result|
+      case search_result.id.kind
+        when 'youtube#video'
+          videos << "https://www.youtube.com/watch?v=" + "#{search_result.id.videoId}"
+        
+      end
+    end
+
+   return videos 
 end
